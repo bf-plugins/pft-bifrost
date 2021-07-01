1a0bf0c928685c61f73e364d5df559c15cb418f1
Danny Price <dancpr@blc00.bl.pvt>
2021-07-01 09:42:10 +0800

    Adding HDF5 writing block

--- test_read_vcs_block.py
+++ test_read_vcs_block.py
@@ -1,6 +1,7 @@
 import bifrost as bf
 from blocks.read_vcs import read_vcs_block
 from blocks.print_stuff import print_stuff_block
+from blocks.h5write import h5write_block
 
 if __name__ == "__main__":
     fn = '../fast-imaging-test/vcs/1164110416_metafits.fits'
@@ -16,9 +17,10 @@ if __name__ == "__main__":
         b_gpu = bf.views.merge_axes(b_gpu, 'coarse_channel', 'fine_channel', label='channel')
         b_gpu = bf.blocks.detect(b_gpu, mode='stokes')
         b_gpu = bf.blocks.reduce(b_gpu, axis='station')
-        b_gpu = bf.blocks.delete_axis(b_gpu, axis='station')
+        b_gpu = bf.views.delete_axis(b_gpu, axis='station')
     b_cpu = bf.blocks.copy(b_gpu, space='system')
-    print_stuff_block(b_cpu)
+    print_stuff_block(b_cpu, n_gulp_per_print=10)
+    h5write_block(b_cpu, n_int_per_file=100)
     
     print("Running pipeline")
     bf.get_default_pipeline().shutdown_on_signals()
